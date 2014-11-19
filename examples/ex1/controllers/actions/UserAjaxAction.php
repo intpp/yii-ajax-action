845b713e8cddca788073b36671ae34b3fbcfeb1e
intpp <intpplus@gmail.com>
2014-11-19 11:03:21 +0200

    examples

--- examples/ex1/controllers/actions/UserAjaxAction.php
+++ examples/ex1/controllers/actions/UserAjaxAction.php
@@ -7,11 +7,33 @@
  */
 class UserAjaxAction extends \intpp\yii\actions\AjaxAction
 {
+    /**
+     * Get list of users
+     */
     public function getList()
     {
         $users = CHtml::listData(User::model()->findAll(), 'id', 'name');
 
         $this->setOutput('result', !empty($users));
         $this->setOutput('users', $users);
+    }
+
+    /**
+     * Return user
+     */
+    public function getUser()
+    {
+        if (!$this->hasParam('userId')) {
+            $this->throwError('Param "userId" is required.');
+        }
+
+        $userId = $this->getParam('userId');
+
+        if (($model = User::model()->findByPk($userId)) === null) {
+            $this->throwError('User not found.');
+        }
+
+        $this->setOutput('result', true);
+        $this->setOutput('user', $model->attributes);
     }
 }
